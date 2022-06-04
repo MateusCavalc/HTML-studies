a0417822ef2c15f7ff7b89a4a53b60ededb162a1
Mateus Cavalcanti <mat.fcavalcanti@gmail.com>
2022-06-04 07:40:23 -0300

    Adiciona diretório de JavaScript e atualiza 'index.html'

---
+++ js/pageControl.js
@@ -0,0 +1,26 @@
+
+function onClick() {
+        var index = $('.main-figure-navigator ul button').index($(this));
+
+        var img = $('#main-img' + (index+1));
+        if(img.css('display') === 'none') {
+                img.css("display", "block");
+                $(this).css('background-color', '#333333');
+
+                $('.img-container img').each(function() {
+                        if(!$(this).attr('id').includes(index+1)) {
+                                $(this).css('display', 'none');
+                        }
+                });
+                $('.main-figure-navigator ul button').each(function() {
+                        if(!$(this).attr('id').includes(index+1)) {
+                                $(this).css('background-color', '#d1d1d1');
+                        }
+                });
+
+        }
+}
+
+$(document).ready(() => {
+        $('.main-figure-navigator ul button').click(onClick);
+});

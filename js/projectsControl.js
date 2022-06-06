7e81b6a6a9955d5d9807f01edb3685016553e05c
Mateus Cavalcanti <mat.fcavalcanti@gmail.com>
2022-06-06 01:19:17 -0300

    debug 'projects.html'

--- js/projectsControl.js
+++ js/projectsControl.js
@@ -1,23 +1,30 @@
 function onClickType() {
+    var el = $(this);
     var type =  $(this).text();
     var cssSeletor;
 
-    if(type == 'all') { cssSeletor = '.page-main .columns figure' }
-    else { cssSeletor = '.page-main .columns figure.' + type }
+    if(type == 'all') {
+        cssSeletor = '.page-main .columns figure';
+        if(!el.hasClass('toggle-type')) { $('.page-main .projects-div nav li:not(:first-child) em').addClass('toggle-type') }
+        else { $('.page-main .projects-div nav li:not(:first-child) em').removeClass('toggle-type') }
+    }
+    else {
+        cssSeletor = '.page-main .columns figure.' + type;
+        var allType = $('.page-main .projects-div nav li:first-child em');
+        if(allType.hasClass('toggle-type')) { allType.removeClass('toggle-type') }
+
+    }
+
+    $(this).toggleClass('toggle-type');
 
     $(cssSeletor).each(function() {
-        if($(this).css('display') === 'none') {
-            $(this).css('display', 'block');
-        }
-        else {
-            $(this).css('display', 'none');
-        }
-    }); 
+        $(this).toggleClass('toggle-figure');
+    });
 
-    if($(this).css('color') == 'rgb(165, 165, 165)') { $(this).css('color', '#262626') }
-    else { $(this).css('color', '#a5a5a5') }
 }
 
 $(document).ready(() => {
+    $('.page-main .projects-div nav em').toggleClass('toggle-type');
+    $('.page-main .projects-div .columns figure').toggleClass('toggle-figure');
     $('.page-main .projects-div > nav em').click(onClickType);
 });
